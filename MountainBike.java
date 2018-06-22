ed9b00b5eaa6ac3e4afdc454a34489eed0be73cc
msamayoa-xp <msamayoa@xpediantsolutions.com>
2018-06-22 14:38:15 -0600

    Added features to MountainBike.

--- MountainBike.java
+++ MountainBike.java
@@ -3,6 +3,8 @@ public class MountainBike extends Bicycle {
     // the MountainBike subclass has
     // one field
     public int seatHeight;
+    private String suspension = "140mm";
+    private int headTube = 70; //angle
 
     // the MountainBike subclass has
     // one constructor
@@ -16,6 +18,19 @@ public class MountainBike extends Bicycle {
     // one method
     public void setHeight(int newValue) {
         seatHeight = newValue;
-    }   
+    } 
+
+    public int getHeadTube(){
+	return headTube;
+    }
+
+    public String getSuspension(){
+ 	return suspension;
+    }
+    
+    public String getSpecs(){
+	System.out.println("Suspension Travel: " + this.getSuspension() );
+	System.out.println("Head Tube angle: " + this.getHeadTube() +"°" );
+    }
 
 }
